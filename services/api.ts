519ddcf9d6dccbfe717609c05079b55db2701850
LangsungKlik.id <yazidbusthomy27@gmail.com>
2026-02-11 10:50:05 +0700

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -5,7 +5,7 @@ import { StudentRegistration } from '../types';
  * GANTI URL DI BAWAH INI dengan URL Web App Anda 
  * yang didapat setelah klik 'Deploy' > 'New Deployment' di Google Apps Script.
  */
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzECTRz9Fh0hEUJplqYMgMTVkv5B_1zJ3JQNX0jYe4ce5mkCGfuVOsLVibYg8cvFsGv/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyJd7pZdTfJJVS035gpEY07XIPEAlYP9ek8r5WY18GAEJ50xeU4db2aBc5Xv1Xvdk1XmQ/exec';
 
 export const api = {
   submitRegistration: async (data: StudentRegistration) => {
